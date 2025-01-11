6350f36c978844335797357f756734558c572df8
choikyungsoo <ryan1954@naver.com>
2025-01-11 20:41:39 +0900

    - :sparkles: feat:  캘린더 페이지 연결

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -2,11 +2,13 @@ import AppBar from "@shared/ui/AppBar.tsx";
 import { HeaderAction } from "@shared/types";
 import IcLeftArrow from "@shared/assets/icon/ic-left-arrow.svg";
 import IcCalendar from "@shared/assets/icon/ic-calendar.svg";
+import { useNavigate } from "react-router";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
   const leftHeaderAction: HeaderAction = { icon: IcLeftArrow, onClick: () => undefined };
   const rightHeaderActionArr: HeaderAction[] = [
-    { icon: IcCalendar, onClick: () => undefined },
+    { icon: IcCalendar, onClick: () => navigate('calendar', {replace: true}) },
   ];
 
   return (
