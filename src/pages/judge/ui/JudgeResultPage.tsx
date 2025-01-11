bb19548881f9700f92167d429bb9a1e49320563c
Tnalxmsk <alstnwkd990@naver.com>
2025-01-12 04:28:54 +0900

    :sparkles: feat: 판결 API response 값 핸들링

--- src/pages/judge/ui/JudgeResultPage.tsx
+++ src/pages/judge/ui/JudgeResultPage.tsx
@@ -1,40 +1,33 @@
 import AppBar from "@shared/ui/AppBar.tsx";
 import IcLeftArrow from "@icon/ic-left-arrow.svg";
-import { HeaderAction } from "@shared/types";
-import { useNavigate } from "react-router";
+import { HeaderAction, JudgeResponse } from "@shared/types";
+import { useLocation, useNavigate } from "react-router";
 import styled from "styled-components";
 import Slider from "@pages/judge/components/Slider.tsx";
 import logo from '@icon/ic-logo.svg';
 
-const example = "남자친구는 반복적으로 술을 마시고 연락을 끊으며 \n" +
-  "외박하는 행동을 하여 여자친구에게 신뢰를 잃게 \n" +
-  "만들었습니다. \n" +
-  "연락이 안 되는 이유가 술 때문이라 할지라도, \n" +
-  "반복적인 행동은 문제를 키웁니다. \n" +
-  "여자친구는 감정적으로 반응하고 연락을 회피한 \n" +
-  "이유는 남자친구의 신뢰를 잃었기 때문이므로, \n" +
-  "감정적으로 이해할 수 있습니다. \n" +
-  "그러나 두 사람 모두 의사소통과 신뢰 회복이\n" +
-  "필요합니다.";
-
 const JudgeResultPage = () => {
+  const { judgeResponse } = useLocation().state as { judgeResponse: JudgeResponse };
   const navigate = useNavigate();
   const leftHeaderAction: HeaderAction = { icon: IcLeftArrow, onClick: () => navigate(-1) };
   const finish = () => {
     navigate('/home');
   };
 
+  const value = judgeResponse.score.split(':')[0];
+  const score = Number(value) * 10;
+
   return (
     <>
       <AppBar leftHeaderAction={leftHeaderAction} title="판결" />
       <Container>
         <ResultContainer>
-          <img src={logo} alt='logo'/>
+          <img src={logo} alt="logo" />
           <BoldText>판결</BoldText>
-          <Judgement>{example}</Judgement>
+          <Judgement>{judgeResponse.c_text}</Judgement>
         </ResultContainer>
         <Section>
-          <Slider />
+          <Slider score={score} />
           <Button onClick={finish}>완료</Button>
         </Section>
       </Container>
