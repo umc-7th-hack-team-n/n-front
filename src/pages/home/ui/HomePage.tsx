74fd4694cbbd57b332385e9ecb223706ab9fd864
devhaeun <lala819010@gmail.com>
2025-01-11 23:47:09 +0900

    feat: 홈 페이지 UI 및 페이지 연결
    홈 페이지 UI 구현 (Pretendard 폰트 미적용 및 이미지 크기 문제 있음)
    페이지 navigate로 연결

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -3,30 +3,38 @@ import { HeaderAction } from "@shared/types";
 import IcLeftArrow from "@shared/assets/icon/ic-left-arrow.svg";
 import IcCalendar from "@shared/assets/icon/ic-calendar.svg";
 import styled from "styled-components";
-import ImgRules from "@shared/assets/image/img-home-rules.png"
+import ImgRules from "@shared/assets/image/img-home-rules.png";
+import ImgHandShake from "@shared/assets/image/img-handshake.png";
+import RightHand from '@shared/assets/image/img-right-hand.png';
+import LeftHand from '@shared/assets/image/img-left-hand.png';
+import { useNavigate } from "react-router";
 
 export const HomePage = () => {
   const leftHeaderAction: HeaderAction = { icon: IcLeftArrow, onClick: () => undefined };
   const rightHeaderActionArr: HeaderAction[] = [
     { icon: IcCalendar, onClick: () => undefined },
   ];
+  const navigate = useNavigate();
 
   return (
     <Container>
       <AppBar leftHeaderAction={leftHeaderAction} rightHeaderActionArr={rightHeaderActionArr} />
-      <ContetnsDiv>
+      <ContetnsDiv onClick={()=>navigate('/')}>
         <ContentImg src={ImgRules} width={'100%'} />
         <Text right={'20px'} bottom={'15px'}>우리가 지킬 10가지</Text>
       </ContetnsDiv>
       <SmallContainer>
-        <SmallContetnsDiv>
+        <SmallContetnsDiv color={'#DEE5FF'}>
+          <img src={LeftHand}/>
+          <img src={RightHand} />
           <Text bottom={'75px'}>판결</Text>
           <SmallText>
             서로 이해가 어려울 땐,<br/>
             제3자의 시선에서 답을 찾아보세요.
           </SmallText>
         </SmallContetnsDiv>
-        <SmallContetnsDiv>
+        <SmallContetnsDiv color={'#FFEAE5'}>
+          <img src={ImgHandShake} />
           <Text bottom={'75px'}>화해</Text>
           <SmallText>
             고요함 속에서 서로를 이해해 보세요.
@@ -46,7 +54,7 @@ const Container = styled.div`
 `
 
 const ContetnsDiv = styled.div`
-  width: 72.046%;
+  width: 90.423%;
   flex-shrink: 0;
   border-radius: 20px;
   margin-top: 27px;
@@ -54,6 +62,7 @@ const ContetnsDiv = styled.div`
   position: relative;
   overflow: hidden;
   aspect-ratio: 1 / 1;
+  cursor: pointer;
 `
 
 const ContentImg = styled.img`
@@ -92,15 +101,15 @@ const SmallText = styled.div`
   font-size: 9px;
   position: absolute;
   text-align: center;
-  left: 2px;
+  left: 50%;
+  transform: translateX(-50%);
   bottom: 35px;
+  width: 100%;
 `
 
 const SmallContainer = styled.div`
-  // width: 100vw;
   width: 100%;
   height: 100vh;
-  // overflow-y: auto;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -108,16 +117,21 @@ const SmallContainer = styled.div`
   padding-bottom: 30px;
 `
 
-const SmallContetnsDiv = styled.div`
+const SmallContetnsDiv = styled.div<{color: string}>`
   background-color: yellow;
-  width: 33.637%;;
-  height: 69.34%;
-  padding: 230px 17px 15px 17px;
-  flex-direction: column;
-  justify-content: flex-end;
+  width: 42.523%;
+  // height: 83.34%;
+  height: 287px;
+  // max-height: 287px;
+  padding: 0 17px 15px 17px;
+  display: flex;
+  // flex-direction: column;
+  justify-content: center;
   align-items: center;
   flex-shrink: 0;
   border-radius: 20px;
   position: relative;
   // aspect-ratio: 1.73 / 1
+  background-color: ${props => props.color};
+  cursor: pointer;
 `
